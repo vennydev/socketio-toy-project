12f6cf18bcc269aacccaebcc0b2890842b3390d2
hayeonkimm <hayeon.data@gmail.com>
2022-06-25 10:06:16 +0900

    업데이트 - index.hs 변경사항 :  socketsJoin 추가

--- index.js
+++ index.js
@@ -13,6 +13,20 @@ io.on('connection', (socket) => {
   });
 });
 
+io.socketsJoin('room1');
+
+// make all Socket instances in the "room1" room join the "room2" and "room3" rooms
+io.in('room1').socketsJoin(['room2', 'room3']);
+
+// make all Socket instances leave the "room1" room
+// io.socketsLeave('room1');
+
+// // make all Socket instances in the "room1" room leave the "room2" and "room3" rooms
+// io.in('room1').socketsLeave(['room2', 'room3']);
+
+// // this also works with a single socket ID
+// io.in(theSocketId).socketsLeave('room1');
+
 http.listen(port, () => {
   console.log(`Socket.IO server running at http://localhost:${port}/`);
 });
